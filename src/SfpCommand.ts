317eff93d6625249f736564068804d755e0dedf6
azlam <sfopsbot@flxbl.io>
2024-12-16 09:59:00 +1100

    fix(sfp): add community edition notice

--- src/SfpCommand.ts
+++ src/SfpCommand.ts
@@ -88,7 +88,7 @@ export default abstract class SfpCommand extends Command {
             SFPLogger.printHeaderLine('',COLOR_HEADER,LoggerLevel.INFO);
             SFPLogger.log(
                 COLOR_HEADER(
-                    `sfp  -- ❤️ by flxbl.io ❤️ -Version:${this.config.version} -Release:${this.config.pjson.release}`
+                    `sfp (community edition)  -- ❤️ by flxbl.io ❤️ -Version:${this.config.version} -Release:${this.config.pjson.release}`
                 )
             );
 
